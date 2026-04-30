dccab0fb6c2382960c3d6f6ab2af29e182fbcd60
kindrick <kuetekindrick@gmail.com>
2026-04-30 06:01:15 +0100

    Fix DBConnection pour FreeSQLDatabase

--- src/main/java/cm/cesb/util/DBConnection.java
+++ src/main/java/cm/cesb/util/DBConnection.java
@@ -6,15 +6,34 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL =
-        "jdbc:mysql://localhost:3306/ces_batcha"
-        + "?useSSL=false"
-        + "&serverTimezone=Africa/Douala"
-        + "&characterEncoding=UTF-8"
-        + "&allowPublicKeyRetrieval=true";
+    private static final String HOST =
+        System.getenv("DB_HOST") != null
+        ? System.getenv("DB_HOST") : "sql3.freesqldatabase.com";
 
-    private static final String USER     = "root";
-    private static final String PASSWORD = "";
+    private static final String PORT =
+        System.getenv("DB_PORT") != null
+        ? System.getenv("DB_PORT") : "3306";
+
+    private static final String DATABASE =
+        System.getenv("DB_NAME") != null
+        ? System.getenv("DB_NAME") : "sql3824750";
+
+    private static final String USER =
+        System.getenv("DB_USER") != null
+        ? System.getenv("DB_USER") : "sql3824750";
+
+    private static final String PASSWORD =
+        System.getenv("DB_PASSWORD") != null
+        ? System.getenv("DB_PASSWORD") : "k8f4LVynQF";
+
+    private static final String URL =
+        "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE
+        + "?useSSL=true"
+        + "&serverTimezone=UTC"
+        + "&characterEncoding=UTF-8"
+        + "&allowPublicKeyRetrieval=true"
+        + "&connectTimeout=30000"
+        + "&socketTimeout=30000";
 
     static {
         try {
