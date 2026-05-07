17bc8747ad524be4a0f88cddc223afdb864fcc7a
kindrick <kuetekindrick@gmail.com>
2026-05-07 08:07:04 +0100

    mise a jour pour railway

--- src/main/java/cm/cesb/util/DBConnection.java
+++ src/main/java/cm/cesb/util/DBConnection.java
@@ -8,27 +8,29 @@ public class DBConnection {
 
     private static final String HOST =
         System.getenv("DB_HOST") != null
-        ? System.getenv("DB_HOST") : "sql3.freesqldatabase.com";
+        ? System.getenv("DB_HOST")
+        : "switchyard.proxy.rlwy.net";
 
     private static final String PORT =
         System.getenv("DB_PORT") != null
-        ? System.getenv("DB_PORT") : "3306";
+        ? System.getenv("DB_PORT") : "25971";
 
     private static final String DATABASE =
         System.getenv("DB_NAME") != null
-        ? System.getenv("DB_NAME") : "sql3824750";
+        ? System.getenv("DB_NAME") : "railway";
 
     private static final String USER =
         System.getenv("DB_USER") != null
-        ? System.getenv("DB_USER") : "sql3824750";
+        ? System.getenv("DB_USER") : "root";
 
     private static final String PASSWORD =
         System.getenv("DB_PASSWORD") != null
-        ? System.getenv("DB_PASSWORD") : "k8f4LVynQF";
+        ? System.getenv("DB_PASSWORD")
+        : "ToUMmVKCkohdKYagaNWyTsmlhudnMsEf";
 
     private static final String URL =
         "jdbc:mysql://" + HOST + ":" + PORT + "/" + DATABASE
-        + "?useSSL=true"
+        + "?useSSL=false"
         + "&serverTimezone=UTC"
         + "&characterEncoding=UTF-8"
         + "&allowPublicKeyRetrieval=true"
